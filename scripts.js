0cc0028f9ae4ba0716b6533c71bd43a0533eae36
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-01-14 12:00:31 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,65 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function deleteNumber() {
+    currentOperand = currentOperand.toString().slice(0, -1);
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+    updateDisplay();
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerText = `${previousOperand} ${operation || ''} ${currentOperand}`;
+}
